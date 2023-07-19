a96204da03121018ffc99acb9e67414ecc7500ae
Deltager <teknisk+GDC2023-GR6@game.unf.dk>
2023-07-19 10:39:56 +0200

    made combat cooldown work

--- TheUnityProject/Assets/Scripts/PlayerCombatScript.cs
+++ TheUnityProject/Assets/Scripts/PlayerCombatScript.cs
@@ -122,8 +122,6 @@ public class PlayerCombatScript : MonoBehaviour
                 SwordBufferTimer = SwordBufferCooldown;
             }
             
-           
-            
 
         }
         
